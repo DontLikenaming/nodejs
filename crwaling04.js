81454971273b5552850b7c28fe23aa01812ae778
DontLikenaming <edirna21@gmail.com>
2023-02-10 17:48:10 +0900

    data.go.kr - 미세먼지 관련 데이터 출력 (json)

---
+++ crwaling04.js
@@ -0,0 +1,63 @@
+// 미세먼지 공공데이터를 이용해서 특정 지역의 미세먼지 정보 출력
+// https://apis.data.go.kr/B552584/ArpltnInforInqireSvc/getCtprvnRltmMesureDnsty
+// ?serviceKey=79BlhIbotMxJTKhCQdR3iVwJf2Z%2FpRVSF%2FWqdETOTwQkdAteE0Nf43hlXP9VpszC7yYGloLYd5xUpfJlnIi9Bw%3D%3D&returnType=json&numOfRows=100&pageNo=1&sidoName=서울&ver=1.0
+
+
+// 사용할 패키지 가져오기 : require('패키지 이름') <-` 말고 '를 써야 함
+const axios = require('axios');
+const cheerio = require('cheerio');
+
+
+
+async function main() {
+    // 접속할 url, quarystring, 요청 헤더 지정
+    const URL = "http://apis.data.go.kr/B552584/ArpltnInforInqireSvc/getCtprvnRltmMesureDnsty";
+    const params = {'serviceKey':'79BlhIbotMxJTKhCQdR3iVwJf2Z/pRVSF/WqdETOTwQkdAteE0Nf43hlXP9VpszC7yYGloLYd5xUpfJlnIi9Bw==',
+                     'returnType':'json',
+                     'sidoName':'전국',
+                     'numOfRows':500};
+
+    const headers = {'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36 Edg/109.0.1518.78'};
+
+    // axios로 접속해서 대기오염 정보를 받아옴
+    const json = await axios.get(URL, {
+        params : params, headers : headers
+    });
+
+    // 받아온 데이터 확인
+/*    console.log(json.data);*/
+    let items = json.data['response']['body']['items']
+    /*console.log(items);*/
+
+    //미세먼지 정보 출력
+    //pm25Value는 출력 안됨
+    for(let item of items){
+        console.log(item.sidoName, item.stationName, item.pm10Value, item.pm25Value, item.dataTime);
+    }
+
+    /*const dom = cheerio.load(json.data);
+
+    let elements = dom('.book_tit');
+
+
+    elements.each((idx, title)=>{
+        /!*console.log(dom(title).text());*!/
+        titles.push(dom(title).text());
+    })
+
+    elements = dom('.book_writer');
+    elements.each((idx, writer)=>{
+        /!*console.log(dom(writer).text());*!/
+        writers.push(dom(writer).text());
+    })
+
+    elements = dom('.price');
+    elements.each((idx, price)=>{
+        /!*console.log(dom(price).text());*!/
+        prices.push(dom(price).text());
+    })*/
+
+    //저장된 배열 요소 갯수 확인
+    /*console.log(titles.length, writers.length, prices.length);*/
+}
+main();
